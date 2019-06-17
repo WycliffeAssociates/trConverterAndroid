b470da467f2ca42019c05d132d132c167fc8c999
mxaln <murmax82@gmail.com>
2019-06-17 09:28:40 -0400

    Added storage permissions check, change transform button color

--- app/src/main/java/org/wycliffeassociates/converter/MainActivity.java
+++ app/src/main/java/org/wycliffeassociates/converter/MainActivity.java
@@ -1,10 +1,15 @@
 package org.wycliffeassociates.converter;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
@@ -15,15 +20,32 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+    }
 
-        fragment = fm.findFragmentById(R.id.fragment_container);
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-        if(fragment == null) {
-            fragment = new ConverterFragment();
-            ft.replace(R.id.fragment_container, fragment);
-            ft.commit();
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+
+            fragment = fm.findFragmentById(R.id.fragment_container);
+
+            if(fragment == null) {
+                fragment = new ConverterFragment();
+                ft.replace(R.id.fragment_container, fragment);
+                ft.commit();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.grant_permission, Toast.LENGTH_LONG).show();
         }
     }
 }
